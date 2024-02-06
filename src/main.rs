11a806deea1c49e1903b50ac783a8164a414919b
Braden Everson <bradeneverson@gmail.com>
2024-02-06 09:25:52 -0600

    Parallel and timer so we don't make UWM mad

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bronzeville_scrape::uwm::scrape::scrape_sites;
+use std::{thread, time};
 
 fn main() {
-    scrape_sites(0..1000).unwrap();
+    for i in 0..100 {
+        let range = (i*10)..(i+1)*10;
+        scrape_sites(range).unwrap();
+        let time = time::Duration::from_millis(5000);
+        thread::sleep(time);
+    }
 }
--- src/uwm/scrape.rs
+++ src/uwm/scrape.rs
@@ -1,18 +1,20 @@
 use std::{ops::Range, error::Error};
+use rayon::prelude::*;
 
 pub fn scrape_sites(id_range: Range<u32>) -> Result<(), Box<dyn Error>> {
-    for idx in id_range {
-        let go_str = format!("https://collections.lib.uwm.edu/digital/download/collection/san/id/{}/size/full", idx);
-        let mut file = std::fs::File::create(format!("src/img/{}.png", idx)).unwrap();
-        let mut request = reqwest::blocking::get(go_str.clone()).unwrap();
+    let mut iterated_file_download = id_range.into_par_iter()
+        .map(|a| {
+            let go_str = format!("https://collections.lib.uwm.edu/digital/download/collection/san/id/{}/size/full", a);
+            let mut file = std::fs::File::create(format!("src/img/{}.png", a)).unwrap();
+            let mut request = reqwest::blocking::get(go_str.clone()).unwrap();
 
-        if request.text().unwrap().contains("html") {
-            println!("Html :(");
-        } else{
-            println!("Beginning download {}", idx);
-            request = reqwest::blocking::get(go_str).unwrap();
-            request.copy_to(&mut file).unwrap();
-        }
+            request.copy_to(&mut file);
+
+            return file;
+        });
+    let mut res = iterated_file_download.collect::<Vec<_>>();
+    for i in 0..res.len() {
+        println!("{:?}", res[i]);
     }
     Ok(())
 }
